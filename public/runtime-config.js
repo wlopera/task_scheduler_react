84e87378fe411bda7fe3073addb12f1dec2d6b5f
William Lopera <lopera.william@gmail.com>
2023-07-26 10:56:04 -0500

    subir para purblicar

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,4 +1,4 @@
 window["runConfig"] = {
-  // BACKEND_URL: "https://backend-chi-jet.vercel.app/api/",
-  BACKEND_URL: "http://localhost:5000/api/",
+  BACKEND_URL: "https://backend-tau-teal.vercel.app/api/",
+  //BACKEND_URL: "http://localhost:5000/api/",
 };
